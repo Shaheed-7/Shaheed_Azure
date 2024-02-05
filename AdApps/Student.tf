388f6d4220c38b80b71e0ee5b82dd06e714a0290
shaheed shaik <shaikshaheed996@gmail.com>
2024-02-05 11:44:37 +0000

    Added Student.tf

---
+++ AdApps/Student.tf
@@ -0,0 +1,8 @@
+Connect-AzAccount -Tenant "98c29f8a-1be1-454c-a666-50f4804cca9c"
+$sub = Get-AzSubscription
+$sub.Name
+$sub.Id
+$sub.SubscriptionId
+$AdApp= New-AzADApplication -DisplayName "Connection_to_ADO"
+Write-Output "Successfully created the AD App"
+$AdApp.AppId
