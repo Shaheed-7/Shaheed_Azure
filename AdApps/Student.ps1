4b0b2147b9a2fccb638b6c91637b9d64b453fa72
shaheed shaik <shaikshaheed996@gmail.com>
2024-02-07 01:58:21 +0000

    Updated Student.ps1

--- AdApps/Student.ps1
+++ AdApps/Student.ps1
@@ -1,3 +1,5 @@
+Install-Module -Name "Az.Accounts" -Scope CurrentUser -Force -AllowClobber
+Import-Module -Name "Az.Accounts"
 Connect-AzAccount -Tenant "98c29f8a-1be1-454c-a666-50f4804cca9c"
 $sub = Get-AzSubscription
 $sub.Name
