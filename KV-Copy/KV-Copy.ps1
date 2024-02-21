3f3fe99da1bd39725724ceb382d1136366c76da2
shaheed shaik <shaikshaheed996@gmail.com>
2024-02-21 02:26:11 +0000

    Added KV-Copy.ps1

---
+++ KV-Copy/KV-Copy.ps1
@@ -0,0 +1,17 @@
+param(
+    [string]$SourceKV = 'KV-Shaheed',
+    [string]$OriginalSecretName = 'VeryImp',
+    [string]$DestinationKV = 'KV-Shaheed-Copy',
+    [string]$DestinationSecretName = 'VeryImp-copy'
+)
+
+
+try {
+    
+    $sourceSecret = Get-AzKeyVaultSecret -VaultName $SourceKV -Name $OriginalSecretName -ErrorAction Stop
+    $secureSecretValue = $sourceSecret.SecretValue
+    $destinationSecret = Set-AzKeyVaultSecret -VaultName $DestinationKV -Name $DestinationSecretName -SecretValue $secureSecretValue -ErrorAction Stop
+    Write-Output "Secret '$OriginalSecretName' from Key Vault '$SourceKV' copied successfully to Key Vault '$DestinationKV' with name '$DestinationSecretName'."
+} catch {
+    Write-Error "Failed to copy secret: $_"
+}
